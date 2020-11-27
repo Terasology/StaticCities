c665c7ab9a995048da4e39710b789b6537fae0d1
Michael Pollind <mpollind@gmail.com>
2020-11-26 20:12:36 -0800

    feat(JOML): migrate player spawn component and logic
    https://github.com/MovingBlocks/Terasology/pull/4281

--- src/main/java/org/terasology/staticCities/CitySpawner.java
+++ src/main/java/org/terasology/staticCities/CitySpawner.java
@@ -17,12 +17,16 @@
 package org.terasology.staticCities;
 
 import java.util.Collections;
+
+import org.joml.Math;
+import org.joml.RoundingMode;
+import org.joml.Vector2i;
+import org.joml.Vector3f;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.spawner.AbstractSpawner;
+import org.terasology.math.JomlUtil;
 import org.terasology.math.Region3i;
-import org.terasology.math.geom.Vector2i;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.staticCities.sites.Site;
 import org.terasology.staticCities.sites.SiteFacet;
 import org.terasology.world.generation.Region;
@@ -39,17 +43,17 @@ public class CitySpawner extends AbstractSpawner {
     @Override
     public Vector3f getSpawnPosition(World world, EntityRef entity) {
         LocationComponent location = entity.getComponent(LocationComponent.class);
-        Vector3f pos = location.getWorldPosition();
-        Vector2i pos2d = new Vector2i(pos.x, pos.z);
-        Region3i region = Region3i.createFromCenterExtents(pos, new Vector3f(32, 1, 32));
+        Vector3f pos = location.getWorldPosition(new Vector3f());
+        Vector2i pos2d = new Vector2i(Math.roundUsing(pos.x, RoundingMode.FLOOR), Math.roundUsing(pos.z, RoundingMode.FLOOR));
+        Region3i region = Region3i.createFromCenterExtents(JomlUtil.from(pos), JomlUtil.from(new Vector3f(32, 1, 32)));
         Region data = world.getWorldData(region);
         SiteFacet settlementFacet = data.getFacet(SiteFacet.class);
         Vector2i searchPos;
         if (!settlementFacet.getSettlements().isEmpty()) {
             Site closest = Collections.min(settlementFacet.getSettlements(),
-                    (a, b) -> a.getPos().distanceSquared(pos2d) - b.getPos().distanceSquared(pos2d));
+                    (a, b) -> a.getPos().distanceSquared(JomlUtil.from(pos2d)) - b.getPos().distanceSquared(JomlUtil.from(pos2d)));
 
-            searchPos = new Vector2i(closest.getPos()); // TODO: replace as soon as findSpawnPos takes BaseVector2i
+            searchPos = new Vector2i(JomlUtil.from(closest.getPos()));
         } else {
             searchPos = pos2d;
         }
