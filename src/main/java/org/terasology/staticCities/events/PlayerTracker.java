a3a73b3fed558706b5ef352fe16ce8314e5e8859
jdrueckert <jd.rueckert@googlemail.com>
2021-02-06 15:40:05 +0100

    feat(JOML): align with CommonWorld migration (#25)
    * CommonWorld PR: https://github.com/Terasology/CommonWorld/pull/8
    
    * remove unused imports
    * update copyright

--- src/main/java/org/terasology/staticCities/events/PlayerTracker.java
+++ src/main/java/org/terasology/staticCities/events/PlayerTracker.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.staticCities.events;
 
@@ -20,6 +7,7 @@ import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.terasology.commonworld.geom.CircleUtility;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.entity.lifecycleEvents.OnActivatedComponent;
 import org.terasology.entitySystem.event.ReceiveEvent;
@@ -29,13 +17,11 @@ import org.terasology.joml.geom.Circlef;
 import org.terasology.logic.characters.events.OnEnterBlockEvent;
 import org.terasology.logic.console.Console;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.JomlUtil;
 import org.terasology.network.Client;
 import org.terasology.network.NetworkSystem;
 import org.terasology.nui.FontColor;
 import org.terasology.registry.In;
 import org.terasology.staticCities.SettlementComponent;
-import org.terasology.commonworld.geom.CircleUtility;
 import org.terasology.staticCities.settlements.Settlement;
 import org.terasology.staticCities.sites.Site;
 import org.terasology.world.WorldComponent;
--- src/main/java/org/terasology/staticCities/raster/standard/RoundPartRasterizer.java
+++ src/main/java/org/terasology/staticCities/raster/standard/RoundPartRasterizer.java
@@ -1,23 +1,11 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.staticCities.raster.standard;
 
 import org.joml.RoundingMode;
 import org.joml.Vector2i;
+import org.terasology.commonworld.geom.CircleUtility;
 import org.terasology.commonworld.heightmap.HeightMap;
 import org.terasology.joml.geom.Circlef;
 import org.terasology.joml.geom.Rectanglef;
@@ -26,7 +14,6 @@ import org.terasology.staticCities.BlockTheme;
 import org.terasology.staticCities.DefaultBlockType;
 import org.terasology.staticCities.bldg.BuildingPartRasterizer;
 import org.terasology.staticCities.bldg.RoundBuildingPart;
-import org.terasology.commonworld.geom.CircleUtility;
 import org.terasology.staticCities.raster.BuildingPens;
 import org.terasology.staticCities.raster.CheckedPen;
 import org.terasology.staticCities.raster.Pen;
