ed72a25c882a271cd711e72c2de8f46d86858ca2
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:17 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/staticCities/parcels/ParcelFacetProvider.java
+++ src/main/java/org/terasology/staticCities/parcels/ParcelFacetProvider.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.commonworld.Orientation;
 import org.terasology.commonworld.geom.CircleUtility;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.utilities.random.FastRandom;
 import org.terasology.engine.utilities.random.Random;
 import org.terasology.engine.world.block.BlockArea;
@@ -21,6 +20,7 @@ import org.terasology.engine.world.generation.Facet;
 import org.terasology.engine.world.generation.GeneratingRegion;
 import org.terasology.engine.world.generation.Produces;
 import org.terasology.engine.world.generation.Requires;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.joml.geom.Circlef;
 import org.terasology.joml.geom.Rectanglef;
 import org.terasology.math.TeraMath;
@@ -224,7 +224,7 @@ public class ParcelFacetProvider implements ConfigurableFacetProvider {
         }
     }
 
-    private static class ParcelConfiguration implements Component {
+    private static class ParcelConfiguration implements Component<ParcelConfiguration> {
 
         @Range(min = 5f, max = 50f, increment = 1f, precision = 0, description = "The min. parcel length")
         private float minSize = 10;
--- src/main/java/org/terasology/staticCities/sites/SiteFacetProvider.java
+++ src/main/java/org/terasology/staticCities/sites/SiteFacetProvider.java
@@ -1,25 +1,11 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.staticCities.sites;
 
 import org.joml.RoundingMode;
 import org.joml.Vector2i;
 import org.joml.Vector2ic;
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.utilities.procedural.Noise;
 import org.terasology.engine.utilities.procedural.WhiteNoise;
 import org.terasology.engine.world.block.BlockArea;
@@ -31,6 +17,7 @@ import org.terasology.engine.world.generation.Facet;
 import org.terasology.engine.world.generation.GeneratingRegion;
 import org.terasology.engine.world.generation.Produces;
 import org.terasology.engine.world.generation.Requires;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.math.TeraMath;
 import org.terasology.nui.properties.Range;
 import org.terasology.staticCities.terrain.BuildableTerrainFacet;
@@ -155,7 +142,7 @@ public class SiteFacetProvider implements ConfigurableFacetProvider {
         return true;
     }
 
-    private static class SiteConfiguration implements Component {
+    private static class SiteConfiguration implements Component<SiteConfiguration> {
 
         @Range(label = "Minimal town size", description = "Minimal town size in blocks", min = 1, max = 150, increment = 10, precision = 1)
         private int minRadius = 50;
